80d02d21ef541f552188b5df8cdedd814fbd13a4
chizhan9527 <1346885811@qq.com>
2022-12-02 20:14:13 +0800

    实现订阅消息

--- IoT_Mqtt/src/main/java/com/project/iot_mqtt/config/MqttConfig.java
+++ IoT_Mqtt/src/main/java/com/project/iot_mqtt/config/MqttConfig.java
@@ -1,5 +1,6 @@
 package com.project.iot_mqtt.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +12,19 @@ import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+
+import java.util.logging.Logger;
 
 @Configuration
+@Slf4j
 public class MqttConfig {
 
+    private final String clientId = "mqtt_114514";
     /**
      * 1、先创建连接
      * 创建MqttPahoClientFactory，设置MQTT Broker连接属性，如果使用SSL验证，也在这里设置。
@@ -25,19 +33,20 @@ public class MqttConfig {
     @Bean
     public MqttPahoClientFactory mqttClientFactory() {
         DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
-        MqttConnectOptions options = new MqttConnectOptions();
 
+        MqttConnectOptions options = new MqttConnectOptions();
         // 设置代理端的URL地址，可以是多个
         options.setServerURIs(new String[]{"tcp://123.60.110.3:1883"});
         options.setUserName("lilei");
         String passWord = "123456";
         options.setPassword(passWord.toCharArray());
+
         factory.setConnectionOptions(options);
         return factory;
     }
 
     /**
-     * 2、入站通道
+     * 2、入站通道（生产者）
      */
     @Bean
     public MessageChannel mqttInputChannel() {
@@ -46,22 +55,28 @@ public class MqttConfig {
 
     /**
      * 入站
+     * 配置client,监听的topic
+     * MQTT消息订阅绑定（消费者）
      */
     @Bean
-    public MessageProducer inbound() {
-        // Paho客户端消息驱动通道适配器，主要用来订阅主题
-        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter("consumerClient-paho",
-                mqttClientFactory(), "boat", "collector", "battery", "+/sensor");
+    public MqttPahoMessageDrivenChannelAdapter mqttSubscriber() {
+        String [] topics = new String[]{"publish", "test/", "test"};
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(clientId+"_subscribe",
+                mqttClientFactory(), topics);
         adapter.setCompletionTimeout(5000);
-
         // Paho消息转换器
         DefaultPahoMessageConverter defaultPahoMessageConverter = new DefaultPahoMessageConverter();
         // 按字节接收消息
         //defaultPahoMessageConverter.setPayloadAsBytes(true);
         adapter.setConverter(defaultPahoMessageConverter);
-        adapter.setQos(1); // 设置QoS
+        adapter.setQos(1);
         adapter.setOutputChannel(mqttInputChannel());
         return adapter;
+    }
+
+    @Bean
+    public MessageProducer inbound() {
+        return mqttSubscriber();
     }
 
 
@@ -73,22 +88,20 @@ public class MqttConfig {
     // ServiceActivator注解表明：当前方法用于处理MQTT消息，inputChannel参数指定了用于消费消息的channel。
     @ServiceActivator(inputChannel = "mqttInputChannel")
     public MessageHandler handler() {
-        return message -> {
-            String payload = message.getPayload().toString();
-
-            // byte[] bytes = (byte[]) message.getPayload(); // 收到的消息是字节格式
-            String topic = message.getHeaders().get("mqtt_receivedTopic").toString();
-
-            // 根据主题分别进行消息处理。
-            if (topic.matches(".+/sensor")) { // 匹配：1/sensor
-                String sensorSn = topic.split("/")[0];
-                System.out.println("传感器" + sensorSn + ": 的消息： " + payload);
-            } else if (topic.equals("collector")) {
-                System.out.println("采集器的消息：" + payload);
-            } else {
-                System.out.println("丢弃消息：主题[" + topic  + "]，负载：" + payload);
+        return new MessageHandler() {
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                try{
+                    String topic = message.getHeaders().get("mqtt_receivedTopic").toString();
+                    String qos = message.getHeaders().get("mqtt_receivedQos").toString();
+                    String payload = message.getPayload().toString();
+                    log.info("主题: "+topic);
+                    log.info("内容："+payload);
+                    log.info("级别："+qos);
+                }catch(Exception e){
+                    log.error(e.getMessage(),e);
+                }
             }
-
         };
     }
 
@@ -111,9 +124,8 @@ public class MqttConfig {
     @Bean
     @ServiceActivator(inputChannel = "mqttOutboundChannel")
     public MessageHandler outbound() {
-
         // 发送消息和消费消息Channel可以使用相同MqttPahoClientFactory
-        MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler("publishClient", mqttClientFactory());
+        MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler(clientId+"_publish", mqttClientFactory());
         messageHandler.setAsync(true); // 如果设置成true，即异步，发送消息时将不会阻塞。
         messageHandler.setDefaultTopic("command");
         messageHandler.setDefaultQos(1); // 设置默认QoS
--- IoT_Mqtt/src/main/java/com/project/iot_mqtt/controller/MqttController.java
+++ IoT_Mqtt/src/main/java/com/project/iot_mqtt/controller/MqttController.java
@@ -1,10 +1,10 @@
 package com.project.iot_mqtt.controller;
 
 import com.project.iot_mqtt.Entity.MyMessage;
+import com.project.iot_mqtt.config.MqttConfig;
 import com.project.iot_mqtt.service.MqttGateway;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -14,11 +14,21 @@ public class MqttController {
     @Resource
     private MqttGateway mqttGateway;
 
+    @Resource
+    private MqttConfig mqttConfig;
+
     @PostMapping("/send")
     public String send(@RequestBody MyMessage myMessage) {
         // 发送消息到指定主题
         mqttGateway.sendToMqtt(myMessage.getTopic(), 1, myMessage.getContent());
         return "send topic: " + myMessage.getTopic()+ ", message : " + myMessage.getContent();
     }
+
+    @PutMapping("/topic")
+    public String addTopic(@RequestParam("topic")String topic){
+        mqttConfig.mqttSubscriber().addTopic(topic,1);
+        return "ok";
+    }
+
 }
 
